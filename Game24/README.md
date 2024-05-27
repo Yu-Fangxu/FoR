8ec41b552ffb2ac263452ca2c76a486782a8fa38
Jianglai-0023 <90485159+Jianglai-0023@users.noreply.github.com>
2024-05-27 22:52:27 +0800

    game24

---
+++ Game24/README.md
@@ -0,0 +1,20 @@
+# GFlowPlanner for Math Reasoning (Game24)
+
+## Task Introduction
+
+
+
+
+
+## Code
+
+1) **Setup**
+
+`pip install -r requirements.txt`
+
+2. **Quick Start**
+
+Training `./scripts/train.sh`
+
+Test `./scripts/test.sh`
+
---
+++ Game24/scripts/train.sh
@@ -0,0 +1,21 @@
+CUDA_VISIBLE_DEVICES=7 python3 main.py \
+	--seed 12 \
+	--pretrained_model meta-llama/Llama-2-13b-chat-hf \
+    --length 20 \
+	--max-length 20 \
+	--num-iters 2000 \
+	--start 0 \
+	--end 50 \
+	--lr-nll-portion 1.0 \
+    --topk 10 \
+    --output-lgt-temp 1 \
+	--verbose \
+    --straight-through  \
+	--buffer_size 50 \
+	--batch-size 4 \
+	--epoch_nums 5 \
+	--train-data data/train.json\
+	--do_train \
+	--do_test \
+	--test_sample_nums 20 \
+	--fp16 > "logs/train.txt" 
