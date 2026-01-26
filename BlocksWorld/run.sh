9852336ee6b164c0cb6f0d50db9689e37dd71c35
Fangxu Yu <58104156+Yu-Fangxu@users.noreply.github.com>
2026-01-25 20:03:00 -0500

    Update p_buffer_start parameter in run.sh

--- BlocksWorld/run.sh
+++ BlocksWorld/run.sh
@@ -9,7 +9,7 @@ CUDA_VISIBLE_DEVICES=6 python main.py \
     --pretrained_model "meta-llama/Meta-Llama-3-8B" \
     --reward_temp_end 2 \
     --pf_temp_start 4 \
-    --p_buffer_start 0.25 \
+    --p_buffer_start 0.3 \
     --epsilon_start 0.3 \
     --world_model "meta-llama/Meta-Llama-3-8B" \
     --mode "train" > "./logs/step_${step}/output.txt" 2>&1 &
